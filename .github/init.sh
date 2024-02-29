04a44fb4c2aad69b579e42be2d0c5794beef50a7
lkirk <kirklloyd@gmail.com>
2024-02-29 13:50:13 -0600

    move setup py dir, fail fast

--- .github/init.sh
+++ .github/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -exu
+
 ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 REPO_URLNAME=$(basename -s .git `git config --get remote.origin.url`)
 
@@ -7,7 +9,7 @@ REPO_URLNAME=$(basename -s .git `git config --get remote.origin.url`)
 REPO_SNAKE_NAME=$(basename -s .git `git config --get remote.origin.url` | tr '-' '_' | tr '[:upper:]' '[:lower:]')
 REPO_OWNER=$(git config --get remote.origin.url | awk -F ':' '{print $2}' | awk -F '/' '{print $1}')
 
-sed -i -e"s/python_module/$REPO_SNAKE_NAME" python_module/setup.py
+sed -i -e"s/python_module/$REPO_SNAKE_NAME" setup.py
 
 mv python_module "$REPO_SNAKE_NAME"
 
