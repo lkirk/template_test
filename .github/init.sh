af15f355ecb89eb41ed71f47a6046fd2a0370613
lkirk <kirklloyd@gmail.com>
2024-02-29 12:03:01 -0600

    trying some automation

---
+++ .github/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+REPO_URLNAME=$(basename -s .git `git config --get remote.origin.url`)
+
+# snake case name
+REPO_SNAKE_NAME=$(basename -s .git `git config --get remote.origin.url` | tr '-' '_' | tr '[:upper:]' '[:lower:]')
+REPO_OWNER=$(git config --get remote.origin.url | awk -F ':' '{print $2}' | awk -F '/' '{print $1}')
+
+sed -i -e"s/python_module/$REPO_SNAKE_NAME" python_module/setup.py
+
+mv python_module "$REPO_SNAKE_NAME"
+
